9ae7bacfe54be5de75e1c398587ad1795195f81b
nato-x <alvares.renato@hotmail.com>
2020-05-12 15:53:56 -0300

    Requisito 1 completo

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>Front-end Online Store</h1>
     </div>
   );
 }
--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,21 @@
 export async function getCategories() {
-  // implement here
+  fetch('https://api.mercadolibre.com/sites/MLB/categories')
+    .then((data) => data.json())
+    .then((categories) => (categories));
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
-  // implement here
+  if (categoryId && !query) {
+    return fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}`)
+      .then((data) => data.json())
+      .then(({ results }) => results);
+  }
+  if (query && !categoryId) {
+    return fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${query}`)
+      .then((data) => data.json())
+      .then(({ results }) => results);
+  }
+  return fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`)
+    .then((data) => data.json())
+    .then(({ results }) => results);
 }
