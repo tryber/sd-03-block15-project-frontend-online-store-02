73c254bbcfd071cde023a1d9da3c44f19a855de4
nato-x <alvares.renato@hotmail.com>
2020-05-12 19:36:02 -0300

    Fix on cart icon and PR

--- src/components/CartButton.js
+++ src/components/CartButton.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const CartButton = () => (
   <div>
     <Link to="/cart" data-testid="shopping-cart-button">
-      <img alt="shopping cart icon" src="./images/shopping-cart-solid.svg" />
+      <img style={{width: '25px', height: '20px'}}alt="shopping cart icon" src="https://cdn.onlinewebfonts.com/svg/img_290616.png" />
     </Link>
   </div>
 );
