6f23edff383941c24a53701ec279d72745b4f051
nato-x <alvares.renato@hotmail.com>
2020-05-12 16:41:40 -0300

    Removed clg

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import './App.css';
-import * as api from './services/api';
+// import * as api from './services/api';
 
 function App() {
-  console.log(api.getCategories());
   return (
     <div className="App">
       <h1>Front-end Online Store</h1>
