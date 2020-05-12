de2f051e993bce803e58faf19182821085c908fc
Ana Carolina Gomes <gomes.anac03@gmail.com>
2020-05-12 17:24:10 -0300

    Add input para pesquisa

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
-import ProductsList from './ProductsList';
+import ProductsSearch from './ProductsSearch';
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/" component={ProductsList} />
+        <Route path="/" component={ProductsSearch} />
       </Switch>
     </Router>
   );
---
+++ src/ProductsSearch.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+class ProductsSearch extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      search: "",
+    }
+    this.onHandleChange = this.onHandleChange.bind(this);
+  }
+
+  onHandleChange(event) {
+    const { value } = event.target;
+    this.setState({ search: value });
+  }
+
+  render() {
+    return (
+      <div>
+        <input value={this.state.search} onChange={this.onHandleChange} />
+        <p data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default ProductsSearch;
