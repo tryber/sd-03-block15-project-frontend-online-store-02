5e6d8ce194bef5cd6c3358052c8debd8f76e9d1e
leonardodamasceno8 <leonardodamasceno8@users>
2020-05-16 14:57:48 -0300

    mergiado

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,34 +3,6 @@ import React from 'react';
 import CartButton from './CartButton';
 
 class Search extends React.Component {
-<<<<<<< HEAD
-  constructor(props) {
-    super(props);
-    this.state = {
-      searchInput: '',
-      isLoading: false,
-    };
-    this.apiRequest = this.apiRequest.bind(this);
-    this.onHandleChange = this.onHandleChange.bind(this);
-  }
-
-  onHandleChange(event) {
-    const { value } = event.target;
-    this.setState({ searchInput: value });
-  }
-
-  apiRequest() {
-    const { categoryId } = this.props;
-    const { searchInput } = this.state;
-    api.getProductsFromCategoryAndQuery(categoryId, searchInput)
-      .then(({ results }) => {
-        this.props.updateProduct(results, results.length === 0);
-        this.setState({ isLoading: true });
-      });
-  }
-
-=======
->>>>>>> 5decd275a9de5d77f0ed07c1cef6547ace06ce41
   render() {
     const {
       searchInput,
