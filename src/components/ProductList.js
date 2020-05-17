afc8e105452b8e75972af82bb6f87f276401aa62
leonardodamasceno8 <leonardodamasceno8@users>
2020-05-17 17:56:44 -0300

    code fixed

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -6,23 +6,23 @@ class ProductList extends React.Component {
   render() {
     const { products, notFound } = this.props;
     return (
-    <div>
-      <main role="main" className="col-md-9 ml-sm-auto col-lg-10 px-md-4">
-        <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center">
-          {notFound && <p>Nenhum produto foi encontrado</p>}
-          <div className="container">
-            <div className="row" >
-              {products &&
-                products.map((product) => (
-                  <div key={product.id}>
-                    <ProductCard product={product} />
-                  </div>
-                ))}
+      <div>
+        <main role="main" className="col-md-9 ml-sm-auto col-lg-10 px-md-4">
+          <div className="d-flex justify-content-between flex-wrap align-items-center">
+            {notFound && <p>Nenhum produto foi encontrado</p>}
+            <div className="container">
+              <div className="row" >
+                {products &&
+                  products.map((product) => (
+                    <div key={product.id}>
+                      <ProductCard product={product} />
+                    </div>
+                  ))}
+              </div>
             </div>
           </div>
-        </div>
-      </main>
-    </div>
+        </main>
+      </div>
     );
   }
 }
