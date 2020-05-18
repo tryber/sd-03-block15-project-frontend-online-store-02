5fe7315b8cc292d90951b9ccfc684c5d893d15ad
leonardodamasceno8 <leonardodamasceno8@users>
2020-05-18 10:30:04 -0300

    codeclimate fized after pull master

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,51 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-<<<<<<< HEAD
-import Shipping from '../components/Shipping';
-
-class ProductCard extends React.Component {
-  constructor(props) {
-    super(props);
-    this.addToCart = this.addToCart.bind(this);
-    this.removeFromCart = this.removeFromCart.bind(this);
-  }
-
-  addToCart() {
-    const { product } = this.props;
-    const cartItems = JSON.parse(localStorage.getItem('cartItems'));
-    if (cartItems === null) {
-      product.quantity = 1;
-      return localStorage.setItem('cartItems', JSON.stringify([{ ...product }]));
-    }
-    const itemRepetido = cartItems.find((item) => item.id === product.id);
-    if (itemRepetido) {
-      const indexOfItemInCart = cartItems.indexOf(itemRepetido);
-      cartItems[indexOfItemInCart].quantity += 1;
-      return localStorage.setItem('cartItems', JSON.stringify(cartItems));
-    }
-    product.quantity = 1;
-    return localStorage.setItem('cartItems', JSON.stringify([...cartItems, { ...product }]));
-  }
-
-  removeFromCart() {
-    const { product } = this.props;
-    const cartItems = JSON.parse(localStorage.getItem('cartItems'));
-    const itemWithUniqueQuantity = cartItems.find((
-      item,
-    ) => item.id === product.id && item.quantity === 1);
-    if (itemWithUniqueQuantity) {
-      const indexOfUnique = cartItems.indexOf(itemWithUniqueQuantity);
-      cartItems.splice(indexOfUnique, 1);
-      return localStorage.setItem('cartItems', JSON.stringify(cartItems));
-    }
-    const indexToSubtract = cartItems.indexOf(cartItems.find((item) => item.id === product.id));
-    cartItems[indexToSubtract].quantity -= 1;
-    return localStorage.setItem('cartItems', JSON.stringify(cartItems));
-  }
-=======
 import AddCartButton from './AddCartButton';
->>>>>>> 76e3b95751982c3a1dcbf6ece4bb5fc81e9dac73
 
 class ProductCard extends React.Component {
   render() {
