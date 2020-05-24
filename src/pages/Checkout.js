abbdeae210ef6aad0b02c6031543b18206327a48
Ana Carolina Gomes <gomes.anac03@gmail.com>
2020-05-24 17:03:56 -0300

    CC Issues/2

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -2,36 +2,31 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import CheckoutForm from '../components/CheckoutForm';
 
-class Checkout extends React.Component {
-  checkoutCard({ thumbnail, title, price, quantity }, index, quant) {
-    const rigthQuantity = quantity || quant;
-    return (
-      <div key={index}>
-        <img src={thumbnail} alt={title} />
-        <span>{title} </span>
-        <span>{`R$ ${price * rigthQuantity}`}</span>
-      </div>
-    );
-  }
+function checkoutCard ({ thumbnail, title, price, quantity }, index, quant) {
+  const rigthQuantity = quantity || quant;
+  return (
+    <div key={index}>
+      <img src={thumbnail} alt={title} />
+      <span>{title} </span>
+      <span>{`R$ ${price * rigthQuantity}`}</span>
+    </div>
+  );
+}
 
+class Checkout extends React.Component {
   render() {
     const cartItems = JSON.parse(localStorage.getItem('cartItems'));
-    const totalPrice = cartItems.reduce((acc, item) => {
-      if (item.product) {
-        acc += item.product.price * item.quantity;
-      } else {
-        acc += item.price * item.quantity;
-      }
-      return acc;
-    }, 0);
+    let totalPrice = 0;
     return (
       <div>
         <div>
           {cartItems.map((item, index) => {
             if (item.product) {
-              return this.checkoutCard(item.product, index, item.quantity);
+              totalPrice += item.product.price * item.quantity;
+              return checkoutCard(item.product, index, item.quantity);
             }
-            return this.checkoutCard(item, index);
+            totalPrice += item.price * item.quantity;
+            return checkoutCard(item, index);
           })}
           <p>{`TOTAL: R$ ${totalPrice}`}</p>
         </div>
