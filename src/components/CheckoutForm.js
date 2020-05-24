259b4f9e7771ce4fe0e97eff5fde2f6916d9df9f
Ana Carolina Gomes <gomes.anac03@gmail.com>
2020-05-24 14:35:10 -0300

    Refatoração do requisito 12/2

---
+++ src/components/CheckoutForm.js
@@ -0,0 +1,66 @@
+import React from 'react';
+
+class CheckoutForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      email: '',
+      cpf: '',
+      phone: '',
+      cep: '',
+      address: '',
+    };
+    this.updateState = this.updateState.bind(this);
+    this.clearState = this.clearState.bind(this);
+  }
+
+  clearState() {
+    this.setState({
+      name: '',
+      email: '',
+      cpf: '',
+      phone: '',
+      cep: '',
+      address: '',
+    });
+  }
+
+  updateState(event, chave) {
+    this.setState({ [chave]: event.target.value });
+  }
+
+  creatInput(label, type, id, testid, value) {
+    return (
+      <div>
+        <label htmlFor={id}>{label}</label>
+        <input
+          type={type}
+          id={id}
+          data-testid={testid}
+          value={value}
+          onChange={(event) => this.updateState(event, id)}
+        />
+      </div>
+    );
+  }
+
+  render() {
+    const { name, email, cpf, phone, cep, address } = this.state;
+    return (
+      <div>
+        {this.creatInput('Nome completo:', 'text', 'name', 'checkout-fullname', name)}
+        {this.creatInput('Email:', 'email', 'email', 'checkout-email', email)}
+        {this.creatInput('CPF:', 'text', 'cpf', 'checkout-cpf', cpf)}
+        {this.creatInput('Telefone:', 'text', 'phone', 'checkout-phone', phone)}
+        {this.creatInput('CEP:', 'text', 'cep', 'checkout-cep', cep)}
+        {this.creatInput('Endereço:', 'text', 'address', 'checkout-address', address)}
+        <div>
+          <button onClick={this.clearState}>Comprar</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default CheckoutForm;
--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -1,106 +1,9 @@
-import React from "react";
+import React from 'react';
+import CheckoutForm from '../components/CheckoutForm';
 
 class Checkout extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: "",
-      email: "",
-      cpf: "",
-      phone: "",
-      cep: "",
-      address: "",
-    };
-    this.updateState = this.updateState.bind(this);
-    this.clearState = this.clearState.bind(this);
-  }
-
-  updateState(event, chave) {
-    this.setState({ [chave]: event.target.value });
-  }
-
-  clearState() {
-    this.setState({
-      fname: "",
-      email: "",
-      cpf: "",
-      phone: "",
-      cep: "",
-      address: "",
-    });
-  }
-
-  renderForm() {
-    const { name, email, cpf, phone, cep, address } = this.state;
-    return (
-      <form>
-        <label htmlFor="name">
-          Nome completo:
-          <input
-            data-testid="checkout-fullname"
-            type="text"
-            id="name"
-            value={name}
-            onChange={(event) => this.updateState(event, "name")}
-          />
-        </label>
-        <label htmlFor="email">
-          Email:
-          <input
-            data-testid="checkout-email"
-            type="email"
-            id="email"
-            value={email}
-            onChange={(event) => this.updateState(event, "email")}
-          />
-        </label>
-        <label htmlFor="cpf">
-          CPF:
-          <input
-            data-testid="checkout-cpf"
-            type="text"
-            id="cpf"
-            value={cpf}
-            onChange={(event) => this.updateState(event, "cpf")}
-          />
-        </label>
-        <label htmlFor="phone">
-          Telefone:
-          <input
-            data-testid="checkout-phone"
-            type="text"
-            id="phone"
-            value={phone}
-            onChange={(event) => this.updateState(event, "phone")}
-          />
-        </label>
-        <label htmlFor="cep">
-          CEP:
-          <input
-            data-testid="checkout-cep"
-            type="text"
-            id="cep"
-            value={cep}
-            onChange={(event) => this.updateState(event, "cep")}
-          />
-        </label>
-        <label htmlFor="address">
-          Endereço:
-          <input
-            data-testid="checkout-address"
-            type="text"
-            id="address"
-            value={address}
-            onChange={(event) => this.updateState(event, "addrees")}
-          />
-        </label>
-        <button onClick={this.clearState}>Comprar</button>
-      </form>
-    );
-  }
-
   render() {
-    const cartItems = JSON.parse(localStorage.getItem("cartItems"));
+    const cartItems = JSON.parse(localStorage.getItem('cartItems'));
     let totalPrice = 0;
     return (
       <div>
@@ -117,7 +20,7 @@ class Checkout extends React.Component {
           })}
           <p>{`TOTAL: R$ ${totalPrice}`}</p>
         </div>
-        <div>{this.renderForm()}</div>
+        <CheckoutForm />
       </div>
     );
   }
